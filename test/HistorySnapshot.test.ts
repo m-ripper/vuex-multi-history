35954282e81e0cb2d9258d7f77fc7b2597bc892c
m-ripper <max@jovo.tech>
2019-12-08 01:20:46 +0100

    :white_check_mark: Added missing tests

--- test/HistorySnapshot.test.ts
+++ test/HistorySnapshot.test.ts
@@ -1,13 +1,54 @@
 import { HistorySnapshot, UniqueHistorySnapshot } from '../src';
 
-describe('HistorySnapshot', () => {
-  test('HistorySnapshot', () => {
+describe('UniqueHistorySnapshot', () => {
+  test('toSnapshot', () => {
     const snapshot: HistorySnapshot = {
       mutation: 'test',
       stateData: { sum: 2 },
     };
-    const referencableSnapshot = new UniqueHistorySnapshot(1, snapshot);
-    expect(referencableSnapshot.mutation).toBe('test');
-    expect(referencableSnapshot.toSnapshot()).toEqual(snapshot);
+    const uniqueSnapshot = new UniqueHistorySnapshot(1, snapshot);
+    expect(uniqueSnapshot.toSnapshot()).toEqual(snapshot);
+  });
+
+  test('mutation', () => {
+    const snapshot: HistorySnapshot = {
+      mutation: 'test',
+      stateData: { sum: 2 },
+    };
+    const uniqueSnapshot = new UniqueHistorySnapshot(1, snapshot);
+    expect(uniqueSnapshot.mutation).toBe('test');
+  });
+
+  describe('stateData', () => {
+    test('stateData is object', () => {
+      const stateData = { sum: 2 };
+      const snapshot: HistorySnapshot = {
+        mutation: 'test',
+        stateData,
+      };
+      const uniqueSnapshot = new UniqueHistorySnapshot(1, snapshot);
+      expect(uniqueSnapshot.stateData).toEqual({ sum: 2 });
+      expect(uniqueSnapshot.stateData).not.toBe(stateData);
+    });
+
+    test('stateData is array', () => {
+      const stateData = [2];
+      const snapshot: HistorySnapshot = {
+        mutation: 'test',
+        stateData,
+      };
+      const uniqueSnapshot = new UniqueHistorySnapshot(1, snapshot);
+      expect(uniqueSnapshot.stateData).toEqual([2]);
+      expect(uniqueSnapshot.stateData).not.toBe(stateData);
+    });
+
+    test('stateData is not object or array', () => {
+      const snapshot: HistorySnapshot = {
+        mutation: 'test',
+        stateData: 2,
+      };
+      const uniqueSnapshot = new UniqueHistorySnapshot(1, snapshot);
+      expect(uniqueSnapshot.stateData).toEqual(2);
+    });
   });
 });
